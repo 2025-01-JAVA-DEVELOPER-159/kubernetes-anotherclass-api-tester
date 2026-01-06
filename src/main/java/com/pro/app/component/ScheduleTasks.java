9038b2f9bef640e8218210ef8c7e139c41f03f09
academyitwill <academyitwill@gmail.com>
2026-01-06 09:06:08 +0900

    ..

--- src/main/java/com/pro/app/component/ScheduleTasks.java
+++ src/main/java/com/pro/app/component/ScheduleTasks.java
@@ -1,13 +1,15 @@
 package com.pro.app.component;
 
-import com.pro.app.service.DefaultService;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.text.SimpleDateFormat;
+import com.pro.app.service.DefaultService;
 
 @Component
 public class ScheduleTasks {
@@ -20,7 +22,7 @@ public class ScheduleTasks {
 
     @Scheduled(fixedRate = 5000)
     public void reportCurrentTime() {
-        //log.info("The time is now {}", dateFormat.format(new Date()));
+        log.info("The time is now {}", dateFormat.format(new Date()));
         defaultService.datasourceSecretLoad();
     }
 }
